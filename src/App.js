bdb501c7c87e848398974731b2e4b588034acee9
Aida N <nacarinobazaga@gmail.com>
2021-03-16 08:42:53 +0100

    Init 03 props, state and events

--- src/App.js
+++ src/App.js
@@ -5,43 +5,12 @@ import "./App.css";
 export class CharacterCard extends Component {
   constructor(props) {
     super(props);
-    console.log("constructor");
-    this.state = {app: true};
   }
-
-  static getDerivedStateFromProps(props, state) {
-    console.log("derived state from props");
-    return null;
-  }
-
-  componentDidUpdate(prevsProps, prevState, snapshot) {
-    console.log("did update");
-
-  }
-
-  componentWillUnmount(){
-    console.log("will unmount");
-
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log("should update?");
-    return false;
-  }
-
-
-  getSnapshotBeforeUpdate(prevsProps, prevState) {
-    console.log("get snapshot before update");
-
-  }
-
   render() {
     // card definition
     console.log("render");
     return ( 
     <div className="card">
-      {/* a veces se mostrará y a veces no, según una variable*/}
-      { this.state.app ? <App /> : <span></span> }
       <h2 className="char-name">Character name</h2>
       <p>State</p>
       <p>Gender</p>
@@ -50,10 +19,6 @@ export class CharacterCard extends Component {
     );
   }
 
-  componentDidMount() {
-    console.log("did mount");
-    this.setState.app  =false;
-  }
 }
 
 
@@ -64,15 +29,6 @@ export class App extends Component {
         <h1>Rick y Morty</h1>
       </div>
     );
-  }
-
-  componentDidMount(){
-    console.log("app: did mount");
-
-  }
-
-  componentWillUnmount() {
-    console.log("app: will unmount");
   }
 
 }
