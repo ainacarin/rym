3592a0ad5dc2148dcb95a5149879f9993e916de1
Aida N <nacarinobazaga@gmail.com>
2021-03-16 08:36:12 +0100

    Conocimientos básicos e introducción a react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         Aprendiendo REACT
-        </a>
-      </header>
+// Card Characters Component
+export class CharacterCard extends Component {
+  constructor(props) {
+    super(props);
+    console.log("constructor");
+    this.state = {app: true};
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log("derived state from props");
+    return null;
+  }
+
+  componentDidUpdate(prevsProps, prevState, snapshot) {
+    console.log("did update");
+
+  }
+
+  componentWillUnmount(){
+    console.log("will unmount");
+
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("should update?");
+    return false;
+  }
+
+
+  getSnapshotBeforeUpdate(prevsProps, prevState) {
+    console.log("get snapshot before update");
+
+  }
+
+  render() {
+    // card definition
+    console.log("render");
+    return ( 
+    <div className="card">
+      {/* a veces se mostrará y a veces no, según una variable*/}
+      { this.state.app ? <App /> : <span></span> }
+      <h2 className="char-name">Character name</h2>
+      <p>State</p>
+      <p>Gender</p>
+      <p>Chapter</p>
     </div>
-  );
+    );
+  }
+
+  componentDidMount() {
+    console.log("did mount");
+    this.setState.app  =false;
+  }
 }
 
-export default App;
+
+export class App extends Component {
+  render() {
+    return (
+      <div className="rym">
+        <h1>Rick y Morty</h1>
+      </div>
+    );
+  }
+
+  componentDidMount(){
+    console.log("app: did mount");
+
+  }
+
+  componentWillUnmount() {
+    console.log("app: will unmount");
+  }
+
+}
